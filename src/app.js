c8a84e8a6ece1bd79e6611f43c84ec01f4a5b8da
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-20 09:25:39 -0500

    Basic app.component.structure

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import AppComponent from "./components/App.component.js";
+
+const app = document.getElementById('app');
+ReactDOM.render(<AppComponent />, app);

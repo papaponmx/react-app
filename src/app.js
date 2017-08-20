0b659ac1ecbf718896165a646556785f33570c70
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-20 09:58:04 -0500

    Fixed reactDom typo

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ import ReactDom from "react-dom";
 import AppComponent from "./components/app.component.js";
 
 const app = document.getElementById('app');
-ReactDOM.render(<AppComponent />, app);
+ReactDom.render(<AppComponent />, app);
--- src/components/App.component.js
+++ src/components/App.component.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from 'react';
+import Todos from './todos.component.js';
 
 export default class App extends React.Component {
 	constructor(){
 		super();
+		this.state = {
+			name: 'Jaime',
+			todos: [
+				{
+					id: '001',
+					name: 'Take out trash',
+					completed: false
+				},
+				{
+					id: '002',
+					name: 'Meal prep',
+					completed: false
+				},
+				{
+					id: '003',
+					name: 'Groceries',
+					completed: false
+				}
+			]
+		}
 	}
 
 	render(){
 		return (
 			<div>
-				WADUP
+				<Todos />
 			</div>	
 		)
 	}

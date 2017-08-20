c999565a780e18c766043ec6c329d7a9cccb2a06
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-20 10:56:06 -0500

    Moved folder

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
+import {Provider} from "react-redux"; 
 
 import AppComponent from "./components/app.component.js";
-
+import store from './store';
 const app = document.getElementById('app');
-ReactDom.render(<AppComponent />, app);
+ReactDom.render( <Provider store={store}>
+	<AppComponent />
+</Provider>, app);

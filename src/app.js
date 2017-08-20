eb31aebddd96873018164b51addd034db01fb3de
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-20 09:26:22 -0500

    app.component convention

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import AppComponent from "./components/App.component.js";
+import AppComponent from "./components/app.component.js";
 
 const app = document.getElementById('app');
 ReactDOM.render(<AppComponent />, app);

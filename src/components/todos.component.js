0e4b086cdedf93ae8eacfc4c95a65bd50eed8b80
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-20 10:19:49 -0500

    Finished static application

--- src/components/todos.component.js
+++ src/components/todos.component.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Todo from './todo.component';
 
 export default class Todos extends Component {
 	render() {
+		let todoNodes = this.props.todos.map(function(todo) {
+			return (
+				<Todo name={todo.name} key={todo.id}>
+					{todo.name}
+				</Todo>
+			)
+		});
 		return (
 			<div>
-				
+				<h1>{this.props.name} is about TODO something</h1>
+				{todoNodes}			
 			</div>
 		);
 	}
